d0fcc4fd8dc8f2ff0641dbe41583cb1c084e0b5a
Jose/Chelu/Xulo <jjesusramon@cifpfbmoll.eu>
2023-05-20 18:20:27 +0200

    feat(): Conexión con BBDD y Persistencia con JPA

--- getting-started/src/main/java/edu/craptocraft/quickstart/GreetingResource.java
+++ getting-started/src/main/java/edu/craptocraft/quickstart/GreetingResource.java
@@ -1,16 +1,45 @@
 package edu.craptocraft.quickstart;
 
+import java.net.URI;
+
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/dev")
 public class GreetingResource {
+
+    @Inject
+    EntityManager em;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Bienvenido a Quarkus Data";
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createDev(Developer dev) {
+
+        em.persist(dev);
+
+        return Response.created(URI.create("/dev/" + dev.getId())).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Developer getDev(@PathParam("id") Integer id) {
+        return em.find(Developer.class, id);
     }
 }
